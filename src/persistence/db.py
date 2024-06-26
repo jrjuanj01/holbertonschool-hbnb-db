b667b955a139036865671364068c338abc5ffba0
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-26 16:15:12 -0400

    Added database query methods

--- src/persistence/db.py
+++ src/persistence/db.py
@@ -14,30 +14,55 @@
 
 from src.models.base import Base
 from src.persistence.repository import Repository
+from flask import current_app as app
 
 
 class DBRepository(Repository):
     """Dummy DB repository"""
 
     def __init__(self) -> None:
-        """Not implemented"""
+        """Sets configuration for the app"""
+        self.useData = app.config['USE_DATABASE']
 
     def get_all(self, model_name: str) -> list:
-        """Not implemented"""
-        return []
+        """Gets all values of a given model"""
+        if self.useData:
+            return app.db.session.query(model_name).all()
+        else:
+            return []
 
     def get(self, model_name: str, obj_id: str) -> Base | None:
-        """Not implemented"""
-
-    def reload(self) -> None:
-        """Not implemented"""
+        """Retrieves the data of a given model with it's ID"""
+        if self.useData:
+            return app.db.session.query(model_name).filter_by(id=obj_id).first()
+        else:
+            return None
 
     def save(self, obj: Base) -> None:
+        """Saves an instance of a given model """
+        if self.useData:
+            app.db.session.add(obj)
+            app.db.session.commit()
+        else:
+            pass
+    def reload(self) -> None:
         """Not implemented"""
-
+        pass
+    
     def update(self, obj: Base) -> Base | None:
-        """Not implemented"""
-
+        """Updates the data of a given model instance"""
+        if self.useData:
+            app.db.session.add(obj)
+            app.db.session.commit()
+            return obj
+        else:
+            return None
+        
     def delete(self, obj: Base) -> bool:
-        """Not implemented"""
-        return False
+        """Deletes the data of a given model instance"""
+        if self.useData:
+            app.db.session.delete(obj)
+            app.db.session.commit()
+            return True
+        else:
+            return False
