8118339b62403d7b81edd917beecfd22dd7a0451
jrjuanj01 <jrjuanj01@gmail.com>
2024-07-03 10:55:51 -0400

    Removed unauthenticated endpoints

--- src/routes/reviews.py
+++ src/routes/reviews.py
@@ -18,9 +18,7 @@ reviews_bp = Blueprint("reviews", __name__)
 reviews_bp.route("/places/<place_id>/reviews", methods=["POST"])(create_review)
 reviews_bp.route("/places/<place_id>/reviews")(get_reviews_from_place)
 reviews_bp.route("/users/<user_id>/reviews")(get_reviews_from_user)
-
 reviews_bp.route("/reviews", methods=["GET"])(get_reviews)
-
 reviews_bp.route("/reviews/<review_id>", methods=["GET"])(get_review_by_id)
 reviews_bp.route("/reviews/<review_id>", methods=["PUT"])(update_review)
 # reviews_bp.route("/reviews/<review_id>", methods=["DELETE"])(delete_review)
--- src/routes/users.py
+++ src/routes/users.py
@@ -14,8 +14,7 @@ from src.controllers.users import (
 users_bp = Blueprint("users", __name__, url_prefix="/users")
 
 users_bp.route("/", methods=["GET"])(get_users)
-users_bp.route("/", methods=["POST"])(create_user)
-
 users_bp.route("/<user_id>", methods=["GET"])(get_user_by_id)
-users_bp.route("/<user_id>", methods=["PUT"])(update_user)
-users_bp.route("/<user_id>", methods=["DELETE"])(delete_user)
+# users_bp.route("/", methods=["POST"])(create_user)
+# users_bp.route("/<user_id>", methods=["PUT"])(update_user)
+# users_bp.route("/<user_id>", methods=["DELETE"])(delete_user)
